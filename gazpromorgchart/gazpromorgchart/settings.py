fa6bc6dfa40a54b05b964b52181f8dcc466b4ad6
Pavel Okhrim <paokhrim@gmail.com>
2024-08-02 15:57:54 +0300

    В settings.py добавлена настройка CSRF_TRUSTED_ORIGINS.

--- gazpromorgchart/gazpromorgchart/settings.py
+++ gazpromorgchart/gazpromorgchart/settings.py
@@ -9,6 +9,8 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', cast=Csv())
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
