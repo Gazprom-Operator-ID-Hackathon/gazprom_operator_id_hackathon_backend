1fd794ae0978d6af59fe732a1a1ae71203693ed3
d1g-1t <paokhrim@gmail.com>
2024-08-08 10:58:19 +0300

    Исправлен MEDIA_ROOT.

--- gazpromorgchart/gazpromorgchart/settings.py
+++ gazpromorgchart/gazpromorgchart/settings.py
@@ -107,6 +107,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/home/workshop/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
